ba37f2f1261c00b46eeca0605a2b5a363510abd4
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-06-11 16:04:37 +0530

    Add files via upload

---
+++ day1/FirstProgram.java
@@ -0,0 +1,10 @@
+package weekOneSession;
+// ctrl+space---> content assist
+// command + space-> content assist
+public class FirstProgram {
+	public static void main(String[] args) { 
+	    // it is the entry point for the java execution
+		System.out.println("Welcome To Testleaf");  
+		// shortcut key to run as java application -->ctrl+f11	
+		}
+}
